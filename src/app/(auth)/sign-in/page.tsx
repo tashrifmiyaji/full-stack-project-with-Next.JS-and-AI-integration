e738908d12c568bdcb43b696f6258028a9c4f701
tashrif-miyaji <tashrifmiyaji@gmail.com>
2026-03-17 15:13:36 +0600

    feat: implement sign-in page with form handling, validation, and navigation

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,9 +1,107 @@
-import React from 'react'
+'use client'
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useState } from "react";
+import { toast } from "sonner"
+import { useRouter } from "next/navigation";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { signInSchema } from "@/schemas/signInSchema";
+import { signIn } from "next-auth/react";
 
 const page = () => {
-  return (
-    <div>sign-in</div>
-  )
+    const router = useRouter()
+
+    //
+    const form = useForm<z.infer<typeof signInSchema>>({
+        resolver: zodResolver(signInSchema),
+        defaultValues: {
+            identifier: '',
+            password: ''
+        }
+    })
+
+    const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+        const result = await signIn("credentials", {
+            redirect: false,
+            identifier: data.identifier,
+            password: data.password
+        })
+
+        if (result?.error) {
+            toast("Login Failed", {
+                description: "Incorrect credential!"
+            })
+        }
+
+        if (result?.url) {
+            router.replace('/dashboard')
+        }
+    }
+
+    return (
+        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+                <div className="text-center">
+                    <h1 className="text-4xl font-extrabold">Join Mystery Message</h1>
+                    <p className="mb-4">Sign In to start your anonymous adventure.</p>
+                </div>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+
+                        <FormField
+                            name="identifier"
+                            control={form.control}
+                            render={({ field }) => {
+                                return (
+                                    <FormItem>
+                                        <FormLabel>email or username</FormLabel>
+
+                                        <FormControl>
+                                            <Input placeholder="email/username" {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )
+                            }}
+                        />
+                        <FormField
+                            name="password"
+                            control={form.control}
+                            render={({ field }) => {
+                                return (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+
+                                        <FormControl>
+                                            <Input type="password" placeholder="password" {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )
+                            }}
+                        />
+                        <div className="flex justify-end">
+                            <Button type="submit">
+                                Signin
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
+                <div className="text-center mt-4">
+                    <p>
+                        Not registered?{' '}
+                        <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">Sign up</Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default page
