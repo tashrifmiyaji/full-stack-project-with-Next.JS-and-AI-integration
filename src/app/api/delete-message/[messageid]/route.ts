cd22abdda1d9d06c8513b3bfa7ddc8afb2fd98ed
tashrif-miyaji <tashrifmiyaji@gmail.com>
2026-03-26 12:05:28 +0600

    feat: implement message deletion functionality with confirmation dialog and API integration

---
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -0,0 +1,56 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+import { User } from "next-auth";
+
+export async function DELETE(
+	req: Request,
+	{ params }: { params: { messageid: any } },
+) {
+	const messageId = params.messageid;
+	await dbConnect();
+	const session = await getServerSession(authOptions);
+	const user: User = session?.user;
+
+	if (!session || !session.user) {
+		return Response.json(
+			{
+				success: false,
+				message: "Not Authenticated!",
+			},
+			{ status: 401 },
+		);
+	}
+	try {
+		const updatedResult = await UserModel.updateOne(
+			{ _id: user._id },
+			{ $pull: { messages: { _id: messageId } } },
+		);
+		if (updatedResult.modifiedCount == 0) {
+			return Response.json(
+				{
+					success: false,
+					message: "Message not found or already delete!",
+				},
+				{ status: 404 },
+			);
+		}
+		return Response.json(
+			{
+				success: true,
+				message: "Message Deleted!",
+			},
+			{ status: 200 },
+		);
+	} catch (error) {
+		console.log("error in delete message route!", error);
+		return Response.json(
+			{
+				success: false,
+				message: "error deleting message!",
+			},
+			{ status: 500 },
+		);
+	}
+}
---
+++ src/components/MessageCard.tsx
@@ -0,0 +1,74 @@
+'use client'
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "./ui/button";
+import { X } from "lucide-react";
+import { Message } from "@/model/User.model";
+import { toast } from "sonner";
+import axios from "axios";
+import { ApiResponse } from "@/types/apiResponse";
+
+
+type MessageCardProps = {
+    message: Message,
+    onMessageDelete: (messageId: any) => void
+}
+
+const MessageCard = ({message, onMessageDelete}: MessageCardProps) => {
+    const handleDeleteConfirm = async () => {
+        const response = await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`)
+        toast(response.data.message,)
+        onMessageDelete(message._id)
+    }
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+
+                <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                        <Button variant="destructive"><X className="w-5 h-5"/></Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                This action cannot be undone. This will permanently delete your
+                                account from our servers.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
+
+                <CardDescription>Card Description</CardDescription>
+                <CardAction>Card Action</CardAction>
+            </CardHeader>
+            <CardContent>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default MessageCard
